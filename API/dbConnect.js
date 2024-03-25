4789f2155fa5607929fbced971a8c386c901a84e
nikoicarl <cnikoi@logig.tech>
2024-03-25 16:08:22 -0700

    data connection added

---
+++ API/dbConnect.js
@@ -0,0 +1,19 @@
+module.exports = function dataBase(mysql) {
+    const dbcon = mysql.createConnection({
+        host     : 'localhost',
+        user     : 'root',
+        password : '',
+        database : 'gym_membership_db'
+    });
+
+    //Checking for database connection
+    dbcon.connect(function(err) {
+        if (err) {
+        console.error('Database is not connected..');
+        return;
+        }   
+        console.log('Database connected...');
+    });
+
+    return dbcon;
+}
--- API/index.js
+++ API/index.js
@@ -2,10 +2,16 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 
-app.listen(8888,function()
+var dbConnect = require('./dataConnect.js');
+
+// database connection
+var dataBase = dbConnect(mysql);
+
+app.listen(5010,function()
 {
     console.log("Server Started and Running ...");
 });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
