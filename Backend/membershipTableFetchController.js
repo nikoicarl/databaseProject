d1d90ce37988b30e90f6b967a70ca6055becd05c
nikoicarl <cnikoi@logig.tech>
2024-03-28 19:41:43 -0700

    table fetch updated

--- Backend/membershipTableFetchController.js
+++ Backend/membershipTableFetchController.js
@@ -4,7 +4,7 @@ module.exports = function (app, dataBase) {
 
     // Fetch All Members Data
     app.get('/getAllMembers', function(request, response) {
-        var sql = "SELECT  fee.amount, fee.due_date, fee.invoice_number, fee.discount, membership_type.memberID, name AS membership_type,employee.fname AS employee_fname, employee.lname AS employee_lname, start_date, end_date, type AS payment_type  FROM `membership_type` INNER JOIN employee ON membership_type.employeeID = employee.employeeID INNER JOIN member ON membership_type.memberID = member.memberID INNER JOIN fee on fee.member_typeID = membership_type.member_typeID;";
+        var sql = "SELECT membership_type.memberID, member.fname AS member_fname, member.lname AS memberlname, member.phone, member.dob, member.age, member.address, name AS membership_type,employee.fname AS employee_fname, employee.lname AS employee_lname, start_date, end_date, type AS payment_type, fee.amount, fee.due_date, fee.invoice_number, fee.discount FROM `membership_type` INNER JOIN employee ON membership_type.employeeID = employee.employeeID INNER JOIN member ON membership_type.memberID = member.memberID INNER JOIN fee on fee.member_typeID = membership_type.member_typeID;";
         dataBase.query(sql, function(error, data){
             if (error) {
                 console.log(error);
